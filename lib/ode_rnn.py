6fb1ed8ed541c1cfd2113ffc88e0b38fd9984289
Yujee Song <rswfaf@gmail.com>
2022-02-22 18:35:58 +0900

    extrap successful

--- lib/ode_rnn.py
+++ lib/ode_rnn.py
@@ -23,78 +23,125 @@ from lib.base_models import Baseline
 
 
 class ODE_RNN(Baseline):
-	def __init__(self, input_dim, latent_dim, device = torch.device("cpu"),
-		z0_diffeq_solver = None, n_gru_units = 100,  n_units = 100,
-		concat_mask = False, obsrv_std = 0.1, use_binary_classif = False,
-		classif_per_tp = False, n_labels = 1, train_classif_w_reconstr = False):
+    def __init__(
+        self,
+        input_dim,
+        latent_dim,
+        device=torch.device("cpu"),
+        z0_diffeq_solver=None,
+        n_gru_units=100,
+        n_units=100,
+        concat_mask=False,
+        obsrv_std=0.1,
+        use_binary_classif=False,
+        classif_per_tp=False,
+        n_labels=1,
+        train_classif_w_reconstr=False,
+    ):
 
-		Baseline.__init__(self, input_dim, latent_dim, device = device, 
-			obsrv_std = obsrv_std, use_binary_classif = use_binary_classif,
-			classif_per_tp = classif_per_tp,
-			n_labels = n_labels,
-			train_classif_w_reconstr = train_classif_w_reconstr)
+        Baseline.__init__(
+            self,
+            input_dim,
+            latent_dim,
+            device=device,
+            obsrv_std=obsrv_std,
+            use_binary_classif=use_binary_classif,
+            classif_per_tp=classif_per_tp,
+            n_labels=n_labels,
+            train_classif_w_reconstr=train_classif_w_reconstr,
+        )
 
-		ode_rnn_encoder_dim = latent_dim
-	
-		self.ode_gru = Encoder_z0_ODE_RNN( 
-			latent_dim = ode_rnn_encoder_dim, 
-			input_dim = (input_dim) * 2, # input and the mask
-			z0_diffeq_solver = z0_diffeq_solver, 
-			n_gru_units = n_gru_units, 
-			device = device).to(device)
+        ode_rnn_encoder_dim = latent_dim
 
-		self.z0_diffeq_solver = z0_diffeq_solver
+        self.ode_gru = Encoder_z0_ODE_RNN(
+            latent_dim=ode_rnn_encoder_dim,
+            input_dim=(input_dim) * 2,  # input and the mask
+            z0_diffeq_solver=z0_diffeq_solver,
+            n_gru_units=n_gru_units,
+            device=device,
+        ).to(device)
 
-		self.decoder = nn.Sequential(
-			nn.Linear(latent_dim, n_units),
-			nn.Tanh(),
-			nn.Linear(n_units, input_dim),)
+        self.z0_diffeq_solver = z0_diffeq_solver
 
-		utils.init_network_weights(self.decoder)
+        self.decoder = nn.Sequential(
+            nn.Linear(latent_dim, n_units), nn.Tanh(), nn.Linear(n_units, input_dim),
+        )
 
+        utils.init_network_weights(self.decoder)
 
-	def get_reconstruction(self, time_steps_to_predict, data, truth_time_steps, 
-		mask = None, n_traj_samples = None, mode = None, test = False):
+    def get_reconstruction(
+        self,
+        time_steps_to_predict,
+        data,
+        truth_time_steps,
+        mask=None,
+        n_traj_samples=None,
+        mode=None,
+        test=False,
+    ):
 
-		# if (len(truth_time_steps) != len(time_steps_to_predict)) or (torch.sum(time_steps_to_predict - truth_time_steps) != 0):
-		# 	raise Exception("Extrapolation mode not implemented for ODE-RNN")
+        # if (len(truth_time_steps) != len(time_steps_to_predict)) or (torch.sum(time_steps_to_predict - truth_time_steps) != 0):
+        # 	raise Exception("Extrapolation mode not implemented for ODE-RNN")
 
-		# # time_steps_to_predict and truth_time_steps should be the same 
-		# assert(len(truth_time_steps) == len(time_steps_to_predict))
-		# assert(mask is not None)
-		
-		data_and_mask = data
-		if mask is not None:
-			data_and_mask = torch.cat([data, mask],-1)
+        # # time_steps_to_predict and truth_time_steps should be the same
+        # assert(len(truth_time_steps) == len(time_steps_to_predict))
+        # assert(mask is not None)
 
-		_, _, latent_ys, _, last_std, last_ti = self.ode_gru.run_odernn(
-			data_and_mask, truth_time_steps, run_backwards = False)
+        data_and_mask = data
+        if mask is not None:
+            data_and_mask = torch.cat([data, mask], -1)
+        # print("Get reconstruction")
+        # print("time_step_to_predict shape: ", time_steps_to_predict.shape)
+        # print("data shape: ", data.shape)
+        # print("observed tp shape: ", truth_time_steps.shape)
+        # print("mask shape: ", mask.shape)
+        # print("data with mask shape: ", data_and_mask.shape)
 
-		if test:
-			_, _, latent_ys = self.ode_gru.extrap_odernn(
-			data_and_mask, time_steps_to_predict, latent_ys, last_std, last_ti, self.decoder, run_backwards = False)
-		
-		latent_ys = latent_ys.permute(0,2,1,3)
-		last_hidden = latent_ys[:,:,-1,:]
+        y_i, y_std, latent_ys, _, last_ti = self.ode_gru.run_odernn(
+            data_and_mask, truth_time_steps, run_backwards=False
+        )
 
-			#assert(torch.sum(int_lambda[0,0,-1,:] <= 0) == 0.)
+        if test:
+            y_i, yi_std, latent_ys_pred = self.ode_gru.extrap_odernn(
+                data_and_mask,
+                time_steps_to_predict,
+                y_i,
+                y_std,
+                last_ti,
+                self.decoder,
+                run_backwards=False,
+            )
 
-		outputs = self.decoder(latent_ys)
-		# Shift outputs for computing the loss -- we should compare the first output to the second data point, etc.
-		first_point = data[:,0,:]
-		outputs = utils.shift_outputs(outputs, first_point)
+            latent_ys_pred = latent_ys_pred.permute(0,2,1,3)
+            last_hidden_pred = latent_ys[:,:,-1,:]
+            # print('latent_ys_pred shape:', latent_ys_pred.shape)
+        
 
-		extra_info = {"first_point": (latent_ys[:,:,-1,:], 0.0, latent_ys[:,:,-1,:])}
+        latent_ys = latent_ys.permute(0, 2, 1, 3)
+        last_hidden = latent_ys[:, :, -1, :]
 
-		if self.use_binary_classif:
-			if self.classif_per_tp:
-				extra_info["label_predictions"] = self.classifier(latent_ys)
-			else:
-				extra_info["label_predictions"] = self.classifier(last_hidden).squeeze(-1)
+        # print('latent_ys shape:', latent_ys.shape)
+        
 
-		# outputs shape: [n_traj_samples, n_traj, n_tp, n_dims]
-		return outputs, extra_info
+        # assert(torch.sum(int_lambda[0,0,-1,:] <= 0) == 0.)
 
+        outputs = self.decoder(latent_ys)
+        # Shift outputs for computing the loss -- we should compare the first output to the second data point, etc.
+        first_point = data[:, 0, :]
+        outputs = utils.shift_outputs(outputs, first_point)
 
+        extra_info = {
+            "first_point": (latent_ys[:, :, -1, :], 0.0, latent_ys[:, :, -1, :])
+        }
 
+        if self.use_binary_classif:
+            if self.classif_per_tp:
+                extra_info["label_predictions"] = self.classifier(latent_ys)
+            else:
+                extra_info["label_predictions"] = self.classifier(last_hidden).squeeze(
+                    -1
+                )
+
+        # outputs shape: [n_traj_samples, n_traj, n_tp, n_dims]
+        return outputs, extra_info
 
--- lib/parse_datasets.py
+++ lib/parse_datasets.py
@@ -24,71 +24,95 @@ import random
 
 #####################################################################################################
 def parse_datasets(args, device):
-	
+    def basic_collate_fn(
+        batch, time_steps, args=args, device=device, data_type="train"
+    ):
+        batch = torch.stack(batch)
+        data_dict = {"data": batch, "time_steps": time_steps}
 
-	def basic_collate_fn(batch, time_steps, args = args, device = device, data_type = "train"):
-		batch = torch.stack(batch)
-		data_dict = {
-			"data": batch, 
-			"time_steps": time_steps}
+        data_dict = utils.split_and_subsample_batch(
+            data_dict, args, data_type=data_type
+        )
+        return data_dict
 
-		data_dict = utils.split_and_subsample_batch(data_dict, args, data_type = data_type)
-		return data_dict
+    dataset_name = args.dataset
 
+    n_total_tp = (
+        args.timepoints + args.extrap + args.extraprnn
+    )  # timepoints = 100, extrap = 1 if True
+    max_t_extrap = (
+        args.max_t / args.timepoints * n_total_tp
+    )  # max_t = 5 subsample from the points in the interval [0, max_tp]
 
-	dataset_name = args.dataset
+    ########### 1d datasets ###########
 
-	n_total_tp = args.timepoints + args.extrap
-	max_t_extrap = args.max_t / args.timepoints * n_total_tp
+    # Sampling args.timepoints time points in the interval [0, args.max_t]
+    # Sample points for both training sequence and explapolation (test)
+    distribution = uniform.Uniform(
+        torch.Tensor([0.0]), torch.Tensor([max_t_extrap])
+    )  # Create uniform distribution from the interval [0, max_t_extrap]
+    time_steps_extrap = distribution.sample(torch.Size([n_total_tp - 1]))[
+        :, 0
+    ]  # sample n_total_tp - 1
+    time_steps_extrap = torch.cat(
+        (torch.Tensor([0.0]), time_steps_extrap)
+    )  # add 0.0 to the tp: now total N = n_total_tp
+    time_steps_extrap = torch.sort(time_steps_extrap)[0]
 
+    dataset_obj = None
+    ##################################################################
+    # Sample a periodic function
+    dataset_obj = Periodic_1d(
+        init_freq=None, init_amplitude=1.0, final_amplitude=1.0, final_freq=None, z0=1.0
+    )
 
-	########### 1d datasets ###########
+    ##################################################################
 
-	# Sampling args.timepoints time points in the interval [0, args.max_t]
-	# Sample points for both training sequence and explapolation (test)
-	distribution = uniform.Uniform(torch.Tensor([0.0]),torch.Tensor([max_t_extrap]))
-	time_steps_extrap =  distribution.sample(torch.Size([n_total_tp-1]))[:,0]
-	time_steps_extrap = torch.cat((torch.Tensor([0.0]), time_steps_extrap))
-	time_steps_extrap = torch.sort(time_steps_extrap)[0]
+    if dataset_obj is None:
+        raise Exception("Unknown dataset: {}".format(dataset_name))
 
-	dataset_obj = None
-	##################################################################
-	# Sample a periodic function
-	dataset_obj = Periodic_1d(
-		init_freq = None, init_amplitude = 1.,
-		final_amplitude = 1., final_freq = None, 
-		z0 = 1.)
+    dataset = dataset_obj.sample_traj(
+        time_steps_extrap, n_samples=args.n, noise_weight=args.noise_weight
+    )  # (tp, value) pair
 
-	##################################################################
+    # Process small datasets
+    dataset = dataset.to(device)
+    time_steps_extrap = time_steps_extrap.to(device)
 
-	if dataset_obj is None:
-		raise Exception("Unknown dataset: {}".format(dataset_name))
+    train_y, test_y = utils.split_train_test(
+        dataset, train_fraq=0.8
+    )  # Train and test data are divided into 8:2 ratio
 
-	dataset = dataset_obj.sample_traj(time_steps_extrap, n_samples = args.n, 
-		noise_weight = args.noise_weight)
+    n_samples = len(dataset)
+    input_dim = dataset.size(-1)
 
-	# Process small datasets
-	dataset = dataset.to(device)
-	time_steps_extrap = time_steps_extrap.to(device)
+    batch_size = min(args.batch_size, args.n)
+    # print("train batch size:", batch_size)
+    # print("test batch size: ", args.n)
+    train_dataloader = DataLoader(
+        train_y,
+        batch_size=batch_size,
+        shuffle=False,
+        collate_fn=lambda batch: basic_collate_fn(
+            batch, time_steps_extrap, data_type="train"
+        ),
+    )  # The masking and other
+    test_dataloader = DataLoader(
+        test_y,
+        batch_size=args.n,
+        shuffle=False,
+        collate_fn=lambda batch: basic_collate_fn(
+            batch, time_steps_extrap, data_type="test"
+        ),
+    )
 
-	train_y, test_y = utils.split_train_test(dataset, train_fraq = 0.8) # Train and test data are divided into 8:2 ratio
+    data_objects = {  # "dataset_obj": dataset_obj,
+        "train_dataloader": utils.inf_generator(train_dataloader),
+        "test_dataloader": utils.inf_generator(test_dataloader),
+        "input_dim": input_dim,
+        "n_train_batches": len(train_dataloader),
+        "n_test_batches": len(test_dataloader),
+    }
 
-	n_samples = len(dataset)
-	input_dim = dataset.size(-1)
-
-	batch_size = min(args.batch_size, args.n)
-	train_dataloader = DataLoader(train_y, batch_size = batch_size, shuffle=False,
-		collate_fn= lambda batch: basic_collate_fn(batch, time_steps_extrap, data_type = "train")) # The masking and other 
-	test_dataloader = DataLoader(test_y, batch_size = args.n, shuffle=False,
-		collate_fn= lambda batch: basic_collate_fn(batch, time_steps_extrap, data_type = "test"))
-	
-	data_objects = {#"dataset_obj": dataset_obj, 
-				"train_dataloader": utils.inf_generator(train_dataloader), 
-				"test_dataloader": utils.inf_generator(test_dataloader),
-				"input_dim": input_dim,
-				"n_train_batches": len(train_dataloader),
-				"n_test_batches": len(test_dataloader)}
-
-	return data_objects
-
+    return data_objects
 
